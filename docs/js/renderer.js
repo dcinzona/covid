3f1b8d2ef7a74d0f01580b8dcaf93b03191677b9
Gustavo Tandeciarz <gtandeciarz@acumensolutions.com>
2020-03-23 21:48:11 -0400

    updating size ratios

--- docs/js/renderer.js
+++ docs/js/renderer.js
@@ -23,9 +23,9 @@ define([], function() {
             {
                 type: "size",
                 minDataValue: 1,
-                maxDataValue: 3000,
+                maxDataValue: 40000,
                 minSize: 5,
-                maxSize: 80,
+                maxSize: 90,
                 valueExpression: "$feature.ct * 1",
                 valueExpressionTitle: "Confirmed Cases",
                 valueUnit: "unknown",
@@ -40,9 +40,9 @@ define([], function() {
                 stops: [
                     { value: 1, color: "cyan" },
                     //{ value: 800, color: "#98d1d1" },
-                    { value: 1600, color: "#ffed85" },
+                    { value: 20000, color: "#ffed85" },
                     //{ value: 3500, color: "#df979e" },
-                    { value: 3500, color: "#c80064" }
+                    { value: 40000, color: "#c80064" }
                 ],
                 legendOptions: {
                     showLegend: true
