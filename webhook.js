a373bdc6aeda51aee87e3164a33f276c6963872e
Gustavo Tandeciarz <gtandeciarz@acumensolutions.com>
2020-03-26 12:28:56 -0400

    adding webhook template

---
+++ webhook.js
@@ -0,0 +1,27 @@
+
+
+require("dotenv").config();
+const http = require("http");
+const crypto = require("crypto");
+const exec = require("child_process").exec;
+const SECRET = process.env.WEBHOOK_SECRET;
+const repo = "./";
+
+http.createServer(function(req, res) {
+    req.on("data", function(chunk) {
+      const signature = `sha1=${crypto
+          .createHmac("sha1", SECRET)
+          .update(chunk)
+          .digest("hex")}`;
+      const isAllowed = req.headers["x-hub-signature"] === signature;
+      const body = JSON.parse(chunk);
+      const isMaster = body.ref === "refs/heads/master";
+      console.log(body);
+          console.log('is allowed: ' + isAllowed);
+      if (isAllowed && isMaster) {
+          // do something
+      }
+    });
+
+    res.end();
+}).listen(3001);
