a3c6aeada98019f0020792b0bda0957a24b6143c
Gustavo Tandeciarz <gtandeciarz@acumensolutions.com>
2020-04-24 09:16:32 -0400

    attempting to fix chrome mobile github pages proxy issue

--- docs/js/shared.js
+++ docs/js/shared.js
@@ -1,20 +1,20 @@
 
 // All the code in this module is 
 // enclosed in closure 
-(function(exports) { 
+(function (exports) {
 
-   const apiVersion = "v6";
-   const dataURI = `/api/${apiVersion}/esri.geojson`;
-   const pubURI = '/data/mapdata.json';//`https://covid.gmt.io/data/mapdata.json`;//`https://covid-data.gmt.io${dataURI}`;
-   
+    const apiVersion = "v6";
+    const dataURI = `${gmt_domainRoot}/api/${apiVersion}/esri.geojson`;
+    const pubURI = `${gmt_domainRoot}/data/mapdata.json`;//`https://covid.gmt.io/data/mapdata.json`;//`https://covid-data.gmt.io${dataURI}`;
+
     // Export the function to exports 
     // In node.js this will be exports  
     // the module.exports 
     // In browser this will be function in 
     // the global object sharedModule 
     exports.dataURI = dataURI;
-    exports.apiVersion = apiVersion; 
+    exports.apiVersion = apiVersion;
     exports.pubURI = pubURI;
-       
-})(typeof exports === 'undefined'?  
-            this['sharedConfig']={}: exports); 
+
+})(typeof exports === 'undefined' ?
+    this['sharedConfig'] = {} : exports); 
